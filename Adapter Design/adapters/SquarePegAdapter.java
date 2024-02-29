264abb7b64138850d0399f821a9f0fb74615fa3e
Labib Muntasir <102364849+labib899@users.noreply.github.com>
2024-02-29 22:54:30 +0600

    Add files via upload

---
+++ Adapter Design/adapters/SquarePegAdapter.java
@@ -0,0 +1,20 @@
+package adapters;
+
+import round.*;
+import square.*;
+
+public class SquarePegAdapter extends RoundPeg
+{
+    private SquarePeg peg;
+
+    public SquarePegAdapter(SquarePeg peg) 
+    {
+        this.peg = peg;
+    }
+
+    @Override
+    public double getRadius() 
+    {
+        return (Math.sqrt(Math.pow((peg.getWidth() / 2), 2) * 2));
+    }
+}
